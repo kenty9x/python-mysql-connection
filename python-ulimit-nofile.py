f4bdef77a1ba61434fde6adea257ac2d380d4561
kenty9x <hoahocqn@gmail.com>
2022-08-06 14:11:28 +0200

    Create python-ulimit-nofile.py

---
+++ python-ulimit-nofile.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+files = [] 
+
+try:
+    for f in range(1, 1025):
+        fh = open("test-{0}".format(f), "w")
+        print "Opened file number ...", f 
+        files.append(fh)
+except IOError as e:        
+    print "IOError: ", str(e)
